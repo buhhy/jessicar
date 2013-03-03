56092403e00e6dce39e2876debb5c2c62d112562
unknown <terence.lei@outlook.com>
2013-03-03 00:45:00 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(function() {
+	var $topNavList = $("#topNavList");
+	var $leftNavList = $("#leftNavList");
+
+	var setUpLeftNav = function() {
+		var $leftNavIndicator = $("#leftNavIndicator");
+		var $leftNavPointer = $("#leftNavPointer");
+
+		var $lis = $leftNavList.children("li");
+
+		$lis.each(function() {
+			var $el = $(this).children("a");
+
+			$el.bind({
+				mouseover: function(_e) {
+					$leftNavPointer.animate({
+						"top": $el.position().top
+					}, 100);
+				},
+				click: function(_e) {
+					$leftNavIndicator.fadeTo(150, 0, function() {
+						$leftNavIndicator.css("top", $el.position().top);
+						$leftNavIndicator.fadeTo(150, 1, function() {
+						});
+					})
+				}
+			});
+		});
+	};
+
+	var setUpTopNav = function() {
+		var $topNavIndicator = $("#topNavIndicator");
+
+		var $lis = $topNavList.children("li");
+
+		var setIndicatorPos = function(_$el) {
+			$topNavIndicator.css("left", _$el.position().left + _$el.width()/2);
+			$topNavIndicator.fadeTo(150, 1, function() {
+			});
+		};
+
+		$lis.each(function() {
+			var $el = $(this).children("a");
+
+			$el.bind({
+				click: function(_e) {
+					$topNavIndicator.fadeTo(150, 0, function() {
+						setIndicatorPos($el);
+					})
+				}
+			});
+		});
+		setIndicatorPos($($lis[0]).children("a"));
+	};
+
+	setUpLeftNav();
+	setUpTopNav();
+});
