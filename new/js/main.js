4b6e9bde8179c8a358e89620a6260d6a5e259525
paradiseocean <jessicafung@live.ca>
2016-05-17 23:05:19 -0700

    spruced up home page

--- new/js/main.js
+++ new/js/main.js
@@ -9,6 +9,17 @@ $("#splashProjectsLink").click(function(event){
 	scrollProjects(event);
 })
 
+$("#contactDrawerLink").click(function(event){
+	openContactContainer(event);
+})
+
+$("#projectsLink").click(function(event){
+	body.stop().animate({
+		scrollTop: 0
+	}, '1000', 'swing')
+	openContactContainer(event);
+})
+
 $("#stickyName").click(function(event){
 	event.preventDefault();
 	body.stop().animate({
@@ -18,7 +29,6 @@ $("#stickyName").click(function(event){
 
 $(window).scroll(function() {
 	if (window.scrollY >= $("#splashProjectsLink").offset().top) {
-		console.log("hello");
 		$("#stickyNavContainer").show("slow");
 	} else {
 		$("#stickyNavContainer").hide("slow");
@@ -29,5 +39,12 @@ function scrollProjects(event) {
 	event.preventDefault();
 	body.stop().animate({
 		scrollTop: $("#featuredProjects").offset().top-100
-	}, '1000', 'swing')
+	}, '2000', 'swing')
+}
+
+function openContactContainer(event) {
+	event.preventDefault();
+	$("#splashContainer").toggleClass("slide");
+	$("#contactOptions").toggleClass("flex");
+	$("#contactContainer").toggleClass("show");
 }
